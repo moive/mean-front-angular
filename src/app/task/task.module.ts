7f5f3d3badd8cc1e43b09305a01015d659e53fb9
Moises Velasquez <mvelasquez@pictoric.com>
2022-08-25 09:49:47 -0500

    config routes - add materialize css and design login view

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TaskRoutingModule } from './task-routing.module';
+import { MainComponent } from './pages/main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
     TaskRoutingModule
