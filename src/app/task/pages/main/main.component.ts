fb3dc70aacda2eb018f5b708c43ebc27e6bd3615
Moises Velasquez <mvelasquez@pictoric.com>
2022-08-27 22:19:29 -0500

    fix html navbar

--- src/app/task/pages/main/main.component.ts
+++ src/app/task/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TaskService } from 'src/app/services/task.service';
 
 @Component({
@@ -10,12 +11,17 @@ export class MainComponent implements OnInit {
   tasks: Array<any> = [];
   user: any;
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private router: Router) {}
 
   ngOnInit(): void {
     this.user = this.taskService.user;
     this.taskService.all().subscribe((res) => {
       this.tasks = res.tasks;
     });
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigateByUrl('auth');
   }
 }
